05da156aa1adc84b27f1ecd36d20e0a74ded8eb3
Danil <dev.dg08@google.com>
2024-06-27 18:29:24 +0300

    -add d.ts

--- types/buildWebpack.d.ts
+++
@@ -1,6 +0,0 @@
-type WebpackConfiguration = import("webpack").Configuration;
-import { BuildOptions } from "../config/types/types";
-
-export declare function buildWebpack(
-  options: BuildOptions,
-): WebpackConfiguration;
